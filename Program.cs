7d90c35a394913570464220b726b05024e5eaf46
Daniel Moulson <daniel.moulson@capitalindex.com>
2019-09-04 17:25:05 +0100

    random word selection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace cli_type
 {
@@ -6,7 +8,16 @@ namespace cli_type
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world");
+            var wordCount = 10; //Default test is 10 words
+            var wordPath = "foo/bar.txt";
+
+            //get random set of wards
+            var words = File.ReadAllText(wordPath).Split(' ').OrderBy(x => Guid.NewGuid()).Take(wordCount);
+            foreach(string word in words)
+            {
+                Console.WriteLine(word + " ");
+            }
+
         }
     }
 }
