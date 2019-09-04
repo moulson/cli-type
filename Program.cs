2f5c75103e22566ac5e4c0cfa6ef4c991ec2d46e
Daniel Moulson <daniel.moulson@capitalindex.com>
2019-09-04 09:46:03 +0100

    solution created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cli_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
